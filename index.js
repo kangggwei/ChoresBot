9bcaefe9c49594a30422e40db6a79e30ea5c2f3e
kangggwei <ong.kangwei@dhs.sg>
2020-09-19 21:59:20 +0100

    added ability to edit and delete chores

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ const addChoreTime = new Scene("addChoreTime");
 stage.register(addChoreTime);
 const addChorePeople = new Scene("addChorePeople");
 stage.register(addChorePeople);
+const viewChore = new Scene("viewChore");
+stage.register(viewChore);
+const editChore = new Scene("editChore");
+stage.register(editChore);
 
 bot.use(Telegraf.log());
 bot.use(session());
@@ -53,25 +57,61 @@ bot.start((ctx) => {
   );
 });
 
-bot.action("add_chores", (ctx) => {
-  ctx.reply("Please enter the name of the chore");
-  ctx.scene.enter("addChoreName");
-});
-
-addChoreName.on("text", async (ctx) => {
-  ctx.session.chore = { name: ctx.update.message.text.trim() };
-  ctx.session.message = "Name: " + ctx.session.chore.name + "\n";
+bot.action("main_screen", (ctx) => {
   ctx.reply(
-    `You have added:\n\n${ctx.session.message}\nKey in the number of effort level (out of 10) given to the chore.`,
+    `What would you like to do?`,
     Markup.inlineKeyboard([
-      Markup.callbackButton("Enter new chore instead.", "add_chores"),
+      [
+        Markup.callbackButton("Add Chores", "add_chores"),
+        Markup.callbackButton("View Chores", "view_chores"),
+      ],
+      [
+        Markup.callbackButton("Add Users", "add_users"),
+        Markup.callbackButton("View Users", "view_users"),
+      ],
     ])
       .oneTime()
       .resize()
       .extra()
   );
-  await ctx.scene.leave("addChoreName");
-  ctx.scene.enter("addChoreEffort");
+  ctx.editMessageReplyMarkup();
+});
+
+////////////////////////////////////////
+//                                    //
+//        Adding Chores Scene         //
+//                                    //
+////////////////////////////////////////
+
+bot.action("add_chores", (ctx) => {
+  ctx.reply("Please enter the name of the chore");
+  ctx.scene.enter("addChoreName");
+  ctx.editMessageReplyMarkup();
+});
+
+const addChoreMarkup = [
+  [Markup.callbackButton("Enter new chore instead.", "add_chores")],
+  [Markup.callbackButton("Back to main...", "main_screen")],
+];
+
+addChoreName.on("text", async (ctx) => {
+  ctx.session.chore = { name: ctx.update.message.text.toLowerCase().trim() };
+
+  const existingChore = await Chore.findOne({ name: ctx.session.chore.name });
+
+  if (existingChore) {
+    ctx.reply(
+      `There is a chore called ${ctx.session.chore.name} that already exists. Please enter a new chore name.`
+    );
+  } else {
+    ctx.session.message = "Name: " + ctx.session.chore.name + "\n";
+    ctx.reply(
+      `You have added:\n\n${ctx.session.message}\nKey in the number of effort level (out of 10) given to the chore.`,
+      Markup.inlineKeyboard(addChoreMarkup).oneTime().resize().extra()
+    );
+    await ctx.scene.leave("addChoreName");
+    ctx.scene.enter("addChoreEffort");
+  }
 });
 
 addChoreEffort.on("text", async (ctx) => {
@@ -79,12 +119,7 @@ addChoreEffort.on("text", async (ctx) => {
   ctx.session.message += "Effort: " + ctx.session.chore.effort + "/10\n";
   ctx.reply(
     `You have added:\n\n${ctx.session.message}\nKey in the amount of time (in minutes) given to the chore.`,
-    Markup.inlineKeyboard([
-      Markup.callbackButton("Enter new chore instead.", "add_chores"),
-    ])
-      .oneTime()
-      .resize()
-      .extra()
+    Markup.inlineKeyboard(addChoreMarkup).oneTime().resize().extra()
   );
   await ctx.scene.leave("addChoreEffort");
   ctx.scene.enter("addChoreTime");
@@ -92,18 +127,179 @@ addChoreEffort.on("text", async (ctx) => {
 
 addChoreTime.on("text", async (ctx) => {
   ctx.session.chore.time = parseInt(ctx.update.message.text.trim());
-  ctx.session.message += "Time taken: " + ctx.session.chore.time + "minutes\n";
+  ctx.session.message += "Time taken: " + ctx.session.chore.time + " minutes\n";
   ctx.reply(
-    `You have added:\n\n${ctx.session.message}\nKey in the amount of time (in minutes) given to the chore.`,
+    `You have added:\n\n${ctx.session.message}\nKey in the number of people required for the chore.`,
+    Markup.inlineKeyboard(addChoreMarkup).oneTime().resize().extra()
+  );
+  await ctx.scene.leave("addChoreTime");
+  ctx.scene.enter("addChorePeople");
+});
+
+addChorePeople.on("text", async (ctx) => {
+  ctx.session.chore.people = parseInt(ctx.update.message.text.trim());
+  ctx.session.message += "People: " + ctx.session.chore.people + "\n";
+  ctx.reply(
+    `You have added:\n\n${ctx.session.message}\nWhat do you want to do next?`,
     Markup.inlineKeyboard([
-      Markup.callbackButton("Enter new chore instead.", "add_chores"),
+      [Markup.callbackButton("Add this chore.", "submit_chore")],
+      ...addChoreMarkup,
     ])
       .oneTime()
       .resize()
       .extra()
   );
-  await ctx.scene.leave("addChoreTime");
-  ctx.scene.enter("addChorePeople");
+  await ctx.scene.leave("addChorePeople");
+});
+
+bot.action("submit_chore", async (ctx) => {
+  const myChore = new Chore({
+    name: ctx.session.chore.name,
+    effort: ctx.session.chore.effort,
+    time: ctx.session.chore.time,
+    people: ctx.session.chore.people,
+    assigned: [],
+    isAssigned: false,
+  });
+
+  myChore.save();
+
+  ctx.reply(
+    `The chore has been added!\n\n${ctx.session.message}`,
+    Markup.inlineKeyboard([
+      [Markup.callbackButton("Add another chore.", "add_chores")],
+      [Markup.callbackButton("Do something else.", "main_screen")],
+    ])
+      .oneTime()
+      .resize()
+      .extra()
+  );
+});
+
+////////////////////////////////////////
+//                                    //
+//        Viewing Chores Scene        //
+//                                    //
+////////////////////////////////////////
+
+bot.action("view_chores", async (ctx) => {
+  const existingChores = await Chore.find();
+
+  const choresList = existingChores.map((x) => [
+    Markup.callbackButton(x.name, "<" + x.name),
+  ]);
+
+  if (choresList.length) {
+    ctx.editMessageText(
+      "Which chore would you like to view in detail?",
+      Markup.inlineKeyboard([
+        ...choresList,
+        [Markup.callbackButton("Back to main...", "main_screen")],
+      ])
+        .oneTime()
+        .resize()
+        .extra()
+    );
+    ctx.scene.enter("viewChore");
+  } else {
+    ctx.editMessageText(
+      "There are no chores currently. Please add chores to view them.",
+      Markup.inlineKeyboard([
+        [
+          Markup.callbackButton("Add Chores", "add_chores"),
+          Markup.callbackButton("Back to main...", "main_screen"),
+        ],
+      ])
+        .oneTime()
+        .resize()
+        .extra()
+    );
+  }
+});
+
+const viewChoreMarkup = [
+  [
+    Markup.callbackButton("View chores", "view_chores"),
+    Markup.callbackButton("Back to main...", "main_screen"),
+  ],
+];
+
+viewChore.action(/^</, async (ctx) => {
+  ctx.session.existingChore = await Chore.findOne({
+    name: ctx.update.callback_query.data.replace("<", ""),
+  });
+  ctx.editMessageText(
+    `The chore in detail is:\n
+Name: ${ctx.session.existingChore.name}
+Effort points: ${ctx.session.existingChore.effort}
+Time taken: ${ctx.session.existingChore.time}
+Number of people required: ${ctx.session.existingChore.people}\n
+Do you want to edit any information?`,
+    Markup.inlineKeyboard([
+      [Markup.callbackButton("Name", "<name")],
+      [Markup.callbackButton("Effort Points", "<effort")],
+      [Markup.callbackButton("Time taken", "<time")],
+      [Markup.callbackButton("Number of people required", "<people")],
+      [Markup.callbackButton("Delete this chore", "delete_chore")],
+      ...viewChoreMarkup,
+    ])
+      .oneTime()
+      .resize()
+      .extra()
+  );
+  await ctx.scene.leave("viewChore");
+  ctx.scene.enter("editChore");
+});
+
+editChore.action("delete_chore", async (ctx) => {
+  await Chore.deleteOne({ name: ctx.session.existingChore.name });
+  ctx.editMessageText(
+    `The chore "${ctx.session.existingChore.name}" has been deleted.`,
+    Markup.inlineKeyboard(viewChoreMarkup).oneTime().resize().extra()
+  );
+});
+
+editChore.action(/^</, async (ctx) => {
+  ctx.session.change = ctx.update.callback_query.data.replace("<", "");
+  ctx.reply(`Please key in the new value for "${ctx.session.change}"`);
+  ctx.editMessageReplyMarkup();
+});
+
+editChore.on("text", async (ctx) => {
+  switch (ctx.session.change) {
+    case "effort":
+      ctx.session.value = parseFloat(ctx.update.message.text.trim());
+      await Chore.updateOne(
+        { name: ctx.session.existingChore.name },
+        { effort: ctx.session.value }
+      );
+      break;
+    case "time":
+      ctx.session.value = parseInt(ctx.update.message.text.trim());
+      await Chore.updateOne(
+        { name: ctx.session.existingChore.name },
+        { time: ctx.session.value }
+      );
+      break;
+    case "people":
+      ctx.session.value = parseInt(ctx.update.message.text.trim());
+      await Chore.updateOne(
+        { name: ctx.session.existingChore.name },
+        { people: ctx.session.value }
+      );
+      break;
+    default:
+      ctx.session.value = ctx.update.message.text.toLowerCase().trim();
+      await Chore.updateOne(
+        { name: ctx.session.existingChore.name },
+        { name: ctx.session.value }
+      );
+  }
+  ctx.reply(
+    `"${ctx.session.change}" has been updated to ${ctx.session.value}.`,
+    Markup.inlineKeyboard(viewChoreMarkup).oneTime().resize().extra()
+  );
+  await ctx.scene.leave("editChore");
 });
 
 bot.action("select_date", (ctx) => {
